0afbb6e5d7181284e2f6e1a9b9dc1ee3a1785ee6
Daniel Ferreira <hollowdenis@gmail.com>
2023-02-17 09:41:57 -0300

    templates de pedidos e alteracoes basicas

--- pedido/urls.py
+++ pedido/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('novo_pedido/', views.novo_pedido, name="novo_pedido"),
--- pedido/views.py
+++ pedido/views.py
@@ -1,5 +1,22 @@
+from django.contrib.messages import constants
+from django.contrib import messages
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pedido, Ticket
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def novo_pedido(request):
-    return HttpResponse('Estou no novo_pedido')
+    if request.method == "GET":
+        return render(request, 'novo_pedido.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        descricao = request.POST.get('descricao')
+    
+    pedido = Pedido(
+        usuario=request.user,
+        nome=nome
+    )
+
+    pedido.save()
+    messages.add_message(request, constants.SUCCESS, 'Novo pet cadastrado')
